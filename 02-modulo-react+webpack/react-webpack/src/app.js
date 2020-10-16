1155b4c8685d244068d076192e9b58567b8ae466
Rafael Barros <rafaelbarros.df@gmail.com>
2020-10-16 18:02:44 -0300

    :construction: MRW#10 - Configurando a ferramenta de lint

--- 02-modulo-react+webpack/react-webpack/src/app.js
+++ 02-modulo-react+webpack/react-webpack/src/app.js
@@ -3,9 +3,8 @@
 import React from 'react'
 
 const App = React.createClass({
-    render: function () {
-        return <h1>Aplicação!!!</h1>
-    }
+  render: function () {
+    return <h1>Aplicação!!!</h1>
+  }
 })
- 
 export default App
