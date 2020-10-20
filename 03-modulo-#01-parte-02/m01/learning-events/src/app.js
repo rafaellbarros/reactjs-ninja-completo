1d4659f488ce9176e1bc26055ad1355ddda85cab
Rafael Barros <rafaelbarros.df@gmail.com>
2020-10-20 17:54:31 -0300

    M1#A16 - A prop children

--- 03-modulo-#01-parte-02/m01/learning-events/src/app.js
+++ 03-modulo-#01-parte-02/m01/learning-events/src/app.js
@@ -1,18 +1,16 @@
 'use strict'
 
 import React, { Component } from 'react'
-import Square from './square'
+import Button from './button'
 
 class App extends Component {
   render () {
     return (
-      <div
-        className='container'
-        onClick={(e) => {
-          alert('clickou!!!')
-        }}
-      >
-        <Square />
+      <div className='container'>
+        <Button>
+          <span>Texto </span>
+          outro Texto
+        </Button>
       </div>
     )
   }
