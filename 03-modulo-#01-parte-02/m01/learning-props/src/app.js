013a38107f913c70c454bed4111a885fe30bde61
Rafael Barros <rafaelbarros.df@gmail.com>
2020-10-20 16:59:02 -0300

    :construction: M1#A14 - Problemas ao duplicar a key

--- 03-modulo-#01-parte-02/m01/learning-props/src/app.js
+++ 03-modulo-#01-parte-02/m01/learning-props/src/app.js
@@ -7,12 +7,12 @@ class App extends Component {
   render () {
     return (
       <div className='container'>
-        {['blue', 'red', 'green'].map((square) => (
-          <Square key={square} color={square} />
+        {['blue', 'red', 'blue'].map((square, index) => (
+          <Square key={index} color={square} />
         ))}
       </div>
     )
   }
-}
+}  
 
 export default App
