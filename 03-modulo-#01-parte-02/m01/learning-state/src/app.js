407f8437600b055904c56b91ecef99f323e612db
Rafael Barros <rafaelbarros.df@gmail.com>
2020-10-20 18:52:38 -0300

    :construction: M1#A18 - State

---
+++ 03-modulo-#01-parte-02/m01/learning-state/src/app.js
@@ -0,0 +1,27 @@
+'use strict'
+
+import React, { Component } from 'react'
+
+class App extends Component {
+  constructor () {
+    super()
+    this.state = {
+      text: 'Rafael'
+    }
+  }
+
+  render () {
+    return (
+      <div
+        className='container'
+        onClick={() => this.setState({
+          text: 'Outro texto'
+        })}
+      >
+        {this.state.text}
+      </div>
+    )
+  }
+}
+
+export default App
