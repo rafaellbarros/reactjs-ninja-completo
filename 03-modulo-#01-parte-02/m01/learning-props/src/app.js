2a667c1216e58cd7d4c3392e40238a8fb9d4099d
Rafael Barros <rafaelbarros.df@gmail.com>
2020-10-20 13:33:06 -0300

    :construction: M1#A13 - Conhecendo a prop key

--- 03-modulo-#01-parte-02/m01/learning-props/src/app.js
+++ 03-modulo-#01-parte-02/m01/learning-props/src/app.js
@@ -1,27 +1,18 @@
 'use strict'
 
 import React, { Component } from 'react'
-import Title from './title'
+import Square from './square'
 
 class App extends Component {
   render () {
     return (
       <div className='container'>
-        <Title name='Rafael' lastname='Barros' />
+        {['blue', 'red', 'green'].map((square) => (
+          <Square key={square} color={square} />
+        ))}
       </div>
     )
   }
 }
 
-/*
-const App = React.createClass({
-  render: function () {
-    return (
-      <div className='container'>
-        <Title name='Rafael' lastname='Barros' />
-      </div>
-    )
-  }
-})
-*/
 export default App
