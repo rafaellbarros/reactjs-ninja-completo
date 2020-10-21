234057038ce5b482b394f71cf011d9951137f010
Rafael Barros <rafaelbarros.df@gmail.com>
2020-10-20 22:05:59 -0300

    :construction: M1#A24 - Lifecycle fluxo de atualização (shouldComponentUpdate)

--- 03-modulo-#01-parte-02/m01/learning-state/src/timer.js
+++ 03-modulo-#01-parte-02/m01/learning-state/src/timer.js
@@ -15,6 +15,11 @@ class Timer extends Component {
     console.log('componentWillReceiveProps', this.props, nextProps)
   }
 
+  shouldComponentUpdate (nextProps, nextState) {
+    console.log('shouldComponentUpdate', this.props, nextProps)
+    return this.props.time !== nextProps.time
+  }
+
   componentDidMount () {
     this.timer = setInterval(() => {
       this.setState({ time: this.state.time + 1 })
