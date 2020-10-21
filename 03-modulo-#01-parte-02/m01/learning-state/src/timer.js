4885dac9deba88b043742f2962e2f5a17e1bb2fc
Rafael Barros <rafaelbarros.df@gmail.com>
2020-10-20 22:29:29 -0300

    :construction: M1#A25 - Lifecycle fluxo de atualização (componentWillUpdate)

--- 03-modulo-#01-parte-02/m01/learning-state/src/timer.js
+++ 03-modulo-#01-parte-02/m01/learning-state/src/timer.js
@@ -4,6 +4,7 @@ import React, { Component } from 'react'
 
 class Timer extends Component {
   constructor () {
+    console.log('construtor timer')
     super()
     this.state = {
       time: 0
@@ -12,25 +13,32 @@ class Timer extends Component {
   }
 
   componentWillReceiveProps (nextProps) {
-    console.log('componentWillReceiveProps', this.props, nextProps)
+    console.log('componentWillReceiveProps timer', this.props, nextProps)
   }
 
   shouldComponentUpdate (nextProps, nextState) {
-    console.log('shouldComponentUpdate', this.props, nextProps)
+    // console.log('shouldComponentUpdate timer', this.props, nextProps)
     return this.props.time !== nextProps.time
   }
 
+  componentWillUpdate (nextProps, nextState) {
+    console.log('componentWillUpdate', this.props, nextProps)
+  }
+
   componentDidMount () {
+    console.log('componentDidMount timer')
     this.timer = setInterval(() => {
       this.setState({ time: this.state.time + 1 })
     }, 1000)
   }
 
   componentWillUnmount () {
+    console.log('componentWillUnmount')
     clearInterval(this.timer)
   }
 
   render () {
+    console.log('render timer')
     return <div>Timer: {this.state.time} </div>
   }
 }
