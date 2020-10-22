53271f1015817ae8d5e8893d1abb73911f0e9017
Rafael Barros <rafaelbarros.df@gmail.com>
2020-10-22 02:12:25 -0300

    :construction: M1#A28 - Introdução à formulários no React

---
+++ 03-modulo-#01-parte-02/m01/learning-forms/src/app.js
@@ -0,0 +1,20 @@
+'use strict'
+
+import React, { Component } from 'react'
+
+class App extends Component {
+  render () {
+    return (
+      <div>
+        <form>
+          <input
+            type='text'
+            defaultValue='Valor inicial'
+          />
+        </form>
+      </div>
+    )
+  }
+}
+
+export default App
