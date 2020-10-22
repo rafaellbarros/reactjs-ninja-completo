efb79ac381f82e6eb277302994bf496dd0713125
Rafael Barros <rafaelbarros.df@gmail.com>
2020-10-22 02:35:20 -0300

    :construction: M1#A31 - Formulários (textarea)

--- 03-modulo-#01-parte-02/m01/learning-forms/src/app.js
+++ 03-modulo-#01-parte-02/m01/learning-forms/src/app.js
@@ -6,7 +6,7 @@ class App extends Component {
   constructor () {
     super()
     this.state = {
-      value: '2'
+      value: ''
     }
   }
 
@@ -14,19 +14,14 @@ class App extends Component {
     return (
       <div>
         <form>
-          <select
-            multiple
+          <textarea
             value={this.state.value}
             onChange={(e) => {
               this.setState({
                 value: e.target.value
               })
             }}
-          >
-            <option value='1'>Opção 1</option>
-            <option value='2'>Opção 2</option>
-            <option value='3'>Opção 3</option>
-          </select>
+          />
         </form>
       </div>
     )
