f34d919489f1d906ec9d92846de7508db22e0ecc
Rafael Barros <rafaelbarros.df@gmail.com>
2020-10-22 02:31:35 -0300

    :construction: M1#A30 - Formulários (select e option)

--- 03-modulo-#01-parte-02/m01/learning-forms/src/app.js
+++ 03-modulo-#01-parte-02/m01/learning-forms/src/app.js
@@ -6,8 +6,7 @@ class App extends Component {
   constructor () {
     super()
     this.state = {
-      value: 'Valor inicial',
-      checked: false
+      value: '2'
     }
   }
 
@@ -15,28 +14,19 @@ class App extends Component {
     return (
       <div>
         <form>
-          <input
-            type='text'
+          <select
+            multiple
             value={this.state.value}
             onChange={(e) => {
               this.setState({
                 value: e.target.value
               })
             }}
-          />
-
-          <label>
-            <input
-              type='checkbox'
-              value='my-checkbox'
-              checked={this.state.checked} /* defaultChecked */
-              onChange={(e) => this.setState({ checked: !this.state.checked })}
-            />
-            Checkbox
-          </label>
-
-          <input type='radio' name='rd' value='1' defaultChecked /> Radio 1
-          <input type='radio' name='rd' value='2' /> Radio 2
+          >
+            <option value='1'>Opção 1</option>
+            <option value='2'>Opção 2</option>
+            <option value='3'>Opção 3</option>
+          </select>
         </form>
       </div>
     )
