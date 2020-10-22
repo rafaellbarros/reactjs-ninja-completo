7aa779b92f7ba8fdd2c24fe3a62ba48fd3fa9e8d
Rafael Barros <rafaelbarros.df@gmail.com>
2020-10-22 02:24:06 -0300

    :construction: M1#A29 - Formulários (checkbox e radio)

--- 03-modulo-#01-parte-02/m01/learning-forms/src/app.js
+++ 03-modulo-#01-parte-02/m01/learning-forms/src/app.js
@@ -3,14 +3,40 @@
 import React, { Component } from 'react'
 
 class App extends Component {
+  constructor () {
+    super()
+    this.state = {
+      value: 'Valor inicial',
+      checked: false
+    }
+  }
+
   render () {
     return (
       <div>
         <form>
           <input
             type='text'
-            defaultValue='Valor inicial'
+            value={this.state.value}
+            onChange={(e) => {
+              this.setState({
+                value: e.target.value
+              })
+            }}
           />
+
+          <label>
+            <input
+              type='checkbox'
+              value='my-checkbox'
+              checked={this.state.checked} /* defaultChecked */
+              onChange={(e) => this.setState({ checked: !this.state.checked })}
+            />
+            Checkbox
+          </label>
+
+          <input type='radio' name='rd' value='1' defaultChecked /> Radio 1
+          <input type='radio' name='rd' value='2' /> Radio 2
         </form>
       </div>
     )
