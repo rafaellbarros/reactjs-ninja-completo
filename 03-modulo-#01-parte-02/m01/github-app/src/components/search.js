a2e9f81dc49a9aeb405a23b0f8a37eddda9d0e07
Rafael Barros <rafaelbarros.df@gmail.com>
2020-10-26 01:38:17 -0300

    :construction: M1#A40 - GitHub app - Fazendo o request dos dados do usuário

--- 03-modulo-#01-parte-02/m01/github-app/src/components/search.js
+++ 03-modulo-#01-parte-02/m01/github-app/src/components/search.js
@@ -1,5 +1,6 @@
 'use strict'
 
+import ajax from '@fdaciuk/ajax'
 import React from 'react'
 
 const Search = () => (
@@ -7,6 +8,18 @@ const Search = () => (
     <input
       type='search'
       placeholder='Digite o nome do usuário'
+      onKeyUp={(e) => {
+        const value = e.target.value
+        const keyCode = e.which || e.keyCode
+        const ENTER = 13
+        if (keyCode === ENTER) {
+          ajax().get(`https://api.github.com/users/${value}`)
+            .then((result) => {
+              console.log(result)
+            })
+        }
+        console.log(keyCode)
+      }}
     />
   </div>
 )
