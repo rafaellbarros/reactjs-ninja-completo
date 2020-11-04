59fb91092e964aa62b3a0f3c821239c214583847
Rafael Barros <rafaelbarros.df@gmail.com>
2020-11-04 19:04:08 -0300

    :construction: M1#A46 - GitHub app - loading enquanto estiver buscando os dados

--- 03-modulo-#01-parte-02/m01/github-app/src/components/app-content.js
+++ 03-modulo-#01-parte-02/m01/github-app/src/components/app-content.js
@@ -6,9 +6,10 @@ import Repos from './repos'
 import Search from './search'
 import UserInfo from './user-info'
 
-const AppContent = ({ userinfo, repos, starred, handleSearch, getRepos, getStarred }) => (
+const AppContent = ({ userinfo, repos, starred, isFetching, handleSearch, getRepos, getStarred }) => (
   <div className='app'>
-    <Search handleSearch={handleSearch} />
+    <Search isDisabled={isFetching} handleSearch={handleSearch} />
+    {isFetching && <div>Carregando..</div>}
     {!!userinfo && <UserInfo userinfo={userinfo} />}
     {!!userinfo && <Actions getRepos={getRepos} getStarred={getStarred} />}
     {!!repos.length &&
